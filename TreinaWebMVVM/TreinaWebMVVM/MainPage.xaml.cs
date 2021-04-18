afba3d11ee212eb11ae6bffde63aea17f87e5668
bellakratchei <isabellavk@hotmail.com.br>
2021-04-18 16:08:28 -0300

    Aplicando os conhecimentos adquiridos sobre o pattern MVVM e introdução ao Xamarin Forms

---
+++ TreinaWebMVVM/TreinaWebMVVM/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TreinaWebMVVM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //sliTamanhoTexto.ValueChanged += sliTamanhoTexto_ValueChanged;
+            //sliRotacaoTexto.ValueChanged += sliRotacaoTexto_ValueChanged;
+        }
+
+        private void btnNavegarOutraPagina_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new OutraPagina());
+        }
+
+        //private void sliRotacaoTexto_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    lblTexto.Rotation = sliRotacaoTexto.Value;
+        //    lblRotacaoTexto.Text = $"{sliRotacaoTexto.Value}°";
+        //}
+
+        //private void sliTamanhoTexto_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    lblTexto.Scale = sliTamanhoTexto.Value;
+        //    lblTamanhoTexto.Text = $"x{sliTamanhoTexto.Value}";
+        //}
+
+        ////vai ser executada depois que terminar de mostrar a interface pro usuário
+        //protected override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //    //sliTamanhoTexto.Value = 1;
+        //    sliRotacaoTexto.Value = 0;
+        //    //lblTamanhoTexto.Text = "x1";
+        //    lblRotacaoTexto.Text = "0°";
+
+        //    //lblTexto.Scale = 1;
+        //    lblTexto.Rotation = 0;
+        //}
+    }
+}
---
+++ TreinaWebMVVM/TreinaWebMVVM/ViewModels/OutraPaginaViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TreinaWebMVVM.ViewModels
+{
+    public class OutraPaginaViewModel : INotifyPropertyChanged
+    {    
+        private string _mensagem;
+
+        public string Mensagem
+        {
+            get { return _mensagem; }
+            set 
+            { 
+                _mensagem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _deveExibirMensagem;
+
+        public bool DeveExibirMensagem
+        {
+            get { return _deveExibirMensagem; }
+            set 
+            { 
+                _deveExibirMensagem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ProcessarCommand 
+        { 
+            get
+            {
+                return new Command<string>((cep) =>
+                {
+                    switch(cep.Trim())
+                    {
+                        case "12345-678":
+                            Mensagem = "Você mora perto!";
+                            break;
+                        case "23456-789":
+                            Mensagem = "Você mora longe!";
+                            break;
+                        default:
+                            Mensagem = "Não sabemos onde você mora :(";
+                            break;
+                    }
+                    DeveExibirMensagem = true;
+                }, (cep) =>
+                {
+                    return !string.IsNullOrEmpty(cep);
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
